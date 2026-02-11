4417d58cbb7d80bd541d91d162288e220c40efaf
zidanbarkat <135465245+zidanone@users.noreply.github.com>
2026-02-11 22:26:55 +0100

    Update index.js

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:8000/api/',
+  baseURL: 'portfolio-b4be.onrender.com/api/',
 });
 
 export const getOverview = () => API.get('overview/');
